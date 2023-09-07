be2d87334ee68bab0f9a98d91cb6210768a52fd0
andrewscott <andrewscott2@usf.edu>
2023-09-06 23:09:06 -0400

    second commit

---
+++ ISM6225_ClassDemo1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//test
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Hello World");
+
+            int x;
+            int y;
+            x = 3;
+            y = 4;
+            //print the output of x + y
+            Console.WriteLine(x + y);
+        }
+    }
+}
---
+++ ISM6225_ClassDemo1/README.md
@@ -0,0 +1,2 @@
+# ISM6225_ClassDemo1
+test
